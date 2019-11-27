f4d9b3b689630d47d7a7d30d72b878f2eec3242f
CBoens <43374935+CBoens@users.noreply.github.com>
2019-11-27 13:34:17 +0100

    Update Program.cs

--- UdpModtager/Program.cs
+++ UdpModtager/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace UdpModtager
 {
@@ -6,7 +9,45 @@ namespace UdpModtager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            UdpClient udpClient = new UdpClient(5005);
+
+            IPEndPoint remoteIpEnd = new IPEndPoint(IPAddress.Any, 0); //
+
+            try
+            {
+                Console.WriteLine("Trying to receive client data");
+
+
+                while (true)
+                {
+                    byte[] receiveBytes = udpClient.Receive(ref remoteIpEnd);
+                    string receivedData = Encoding.UTF8.GetString(receiveBytes);
+                    //Object object = Newtonsoft.Json.JsonConvert.DeserializeObject<Object>(receiveBytes);
+                    Console.WriteLine(receivedData);
+                    Console.WriteLine("Data received: " + remoteIpEnd.Port);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+
+
+
+            UdpClient cli = new UdpClient(5005);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                byte[] bytes = cli.Receive(ref ep);
+                Console.WriteLine(Encoding.UTF8.GetString(bytes));
+            }
+                      
         }
     }
 }
